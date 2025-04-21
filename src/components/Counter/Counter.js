2790450e8aa070c5b167c1e4bb43605d27d88f87
Juan Fernando Giraldo Cardona <juanfis@Mac.lan>
2025-04-20 20:33:29 -0500

    Added loading state to avoid displaying wrong values
    Signed-off-by: Juan Fernando Giraldo Cardona <juanfis@Mac.lan>

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,23 +1,25 @@
 'use client';
 import React from 'react';
+import Spinner from '../Spinner/Spinner';
 
 function Counter() {
-  const [count, setCount] = React.useState(0);
+  const [count, setCount] = React.useState(null);
 
   React.useEffect(() => {
     const savedCount = window?.localStorage.getItem('saved-count');
-    if (savedCount) {
-      setCount(Number(Number(savedCount)));
-    }
+
+    setCount(savedCount ? Number(savedCount) : 0);
   }, []);
 
   React.useEffect(() => {
-    window?.localStorage.setItem('saved-count', count);
+    if (typeof count === 'number') {
+      window.localStorage.setItem('saved-count', count);
+    }
   }, [count]);
 
   return (
     <button className='count-btn' onClick={() => setCount(count + 1)}>
-      Count: {count}
+      Count: {typeof count === 'number' ? count : <Spinner />}
     </button>
   );
 }
