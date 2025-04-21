72d5e0976bc6fad213abf6da3801f152dc3d7a48
Juan Fernando Giraldo Cardona <juanfis@Mac.lan>
2025-04-20 20:20:53 -0500

    Hydration issue fixed
    Signed-off-by: Juan Fernando Giraldo Cardona <juanfis@Mac.lan>

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -2,21 +2,21 @@
 import React from 'react';
 
 function Counter() {
-  const [count, setCount] = React.useState(() => {
-    return Number(
-      window.localStorage.getItem('saved-count') || 0
-    );
-  });
+  const [count, setCount] = React.useState(0);
 
   React.useEffect(() => {
-    window.localStorage.setItem('saved-count', count);
+    const savedCount = window?.localStorage.getItem('saved-count');
+    if (savedCount) {
+      setCount(Number(Number(savedCount)));
+    }
+  }, []);
+
+  React.useEffect(() => {
+    window?.localStorage.setItem('saved-count', count);
   }, [count]);
 
   return (
-    <button
-      className="count-btn"
-      onClick={() => setCount(count + 1)}
-    >
+    <button className='count-btn' onClick={() => setCount(count + 1)}>
       Count: {count}
     </button>
   );
